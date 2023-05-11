919ac815ddbf5e12c0f59c9a6afa020830dbb1a0
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-05-11 18:28:18 +0600

    blog

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import './Blog.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Blog = (props) => {
-    const { id, img, author, time, title } = props.blog;
-    console.log(author);
+    const { id, img, author, time, title, authorImage } = props.blog;
+    // console.log(author);
     return (
-        <div>
-            <div>
-                <img src="" alt="" />
+        <div className='blog'>
+            <div className='blog-image'>
+                <img src={img} alt="" />
             </div>
-            <div>
-                <div>
-                    <img src="" alt="" />
-                    <p>author name: <br />15 April (few days ago) </p>
+            <div className='blog-info'>
+                <div className='author-info'>
+                    <img src={authorImage} alt="" />
+                    <div><h5>{author}</h5> <p>15 April (few days ago)</p></div>
                 </div>
-                <p>read time: </p>
+                <div>
+                    <p>{time} min read <a href=""><FontAwesomeIcon icon={faBookmark} /></a> </p>
+                </div>
             </div>
+            <h2>{title}</h2>
+            <a href="">Mark as read</a>
         </div>
     );
 };
