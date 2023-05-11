df2ce2347a560ce84b3223749e78711b854821aa
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-05-11 17:24:52 +0600

    fetch data from fake data base

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Blog = () => {
+const Blog = (props) => {
+    const { id, img, author, time, title } = props.blog;
+    console.log(author);
     return (
         <div>
-            <h1>Blog</h1>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <p>author name: <br />15 April (few days ago) </p>
+                </div>
+                <p>read time: </p>
+            </div>
         </div>
     );
 };
--- src/Components/Library/Library.jsx
+++ src/Components/Library/Library.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Library.css'
 import Blog from '../Blog/Blog';
 const Library = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('db.json')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
+
+
     return (
         <div className='library'>
             <div>
-                <Blog></Blog>
+                {
+                    blogs.map(blog => <Blog
+                        key={blog.id}
+                        blog={blog}
+                    ></Blog>)
+                }
             </div>
             <div>
                 <h1>cart
